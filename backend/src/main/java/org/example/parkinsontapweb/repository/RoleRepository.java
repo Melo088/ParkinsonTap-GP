6dfcd8c2b5bbf55dcd2cd64aa184e0edce04b299
Melo088 <jcml29310@gmail.com>
2026-04-22 09:22:24 -0500

    feat(AUTH1.3): db tables

---
+++ backend/src/main/java/org/example/parkinsontapweb/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package org.example.parkinsontapweb.repository;
+
+import org.example.parkinsontapweb.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByRoleName(String roleName);
+}
